b78e65af4c9868f441aa289220eea89005efaf45
Aaron Moore <x2kicker2x@gmail.com>
2018-02-23 11:37:41 -0500

    Initialized

---
+++ theme/commonColor.js
@@ -0,0 +1,16 @@
+import color from "color";
+
+import { Platform, Dimensions, PixelRatio } from "react-native";
+
+const deviceHeight = Dimensions.get("window").height;
+const deviceWidth = Dimensions.get("window").width;
+const platform = Platform.OS;
+const platformStyle = undefined;
+
+export default {
+  platformStyle,
+  platform,
+  
+  //Text Color
+  textColor: '#000'
+};
